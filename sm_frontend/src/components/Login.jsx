69705cc1f68811d22ac38e06598869231edbe444
xkostolanskym <kostolansky.marek@gmail.com>
2023-04-13 00:00:47 +0200

    login data obj

--- sm_frontend/src/components/Login.jsx
+++ sm_frontend/src/components/Login.jsx
@@ -6,6 +6,18 @@ import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
 
 const Login = () => {
+  const responseGoogle = (response) => {
+    localStorage.setItem("user", JSON.stringify(response.profileObj));
+    const { name, googleId, imageUrl } = response.profileObj;
+
+    const doc = {
+      _id: googleId,
+      _type: "user",
+      userName: name,
+      image: imageUrl,
+    };
+  };
+
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       Login
@@ -25,12 +37,20 @@ const Login = () => {
           </div>
           <div className="shadow-2xl">
             <GoogleLogin
-              clientId=""
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
               render={(renderProps) => (
-                <button type="button" className="bg-mainColor">
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
                   <FcGoogle className="mr-4" /> Sign in with google
                 </button>
               )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
             />
           </div>
         </div>
