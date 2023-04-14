630f8bf573d30c599fc41d534fce68094ebf7323
xkostolanskym <kostolansky.marek@gmail.com>
2023-04-14 23:24:39 +0200

    dependecies update

--- sm_frontend/src/components/Login.jsx
+++ sm_frontend/src/components/Login.jsx
@@ -8,21 +8,21 @@ import logo from "../assets/logowhite.png";
 import { client } from "../client";
 
 const Login = () => {
+  const navigate = useNavigate();
   const responseGoogle = (response) => {
     localStorage.setItem("user", JSON.stringify(response.profileObj));
     const { name, googleId, imageUrl } = response.profileObj;
-
     const doc = {
       _id: googleId,
       _type: "user",
       userName: name,
       image: imageUrl,
     };
-  };
 
-  client.createIfNotExists(doc).then(() => {
-    navigate("/", { replace: true });
-  });
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
 
   return (
     <div className="flex justify-start items-center flex-col h-screen">
