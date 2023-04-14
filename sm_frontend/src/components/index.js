d0c3946bc3cab332caeee205b9eb2fe931b4f734
xkostolanskym <kostolansky.marek@gmail.com>
2023-04-14 23:52:51 +0200

    Home and Pins com

---
+++ sm_frontend/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Sidebar } from "./sidebar";
+export { default as Login } from "./Lidebar";
+export { default as UserProfile } from "./UserProfile";
--- sm_frontend/src/container/Home.jsx
+++ sm_frontend/src/container/Home.jsx
@@ -1,3 +1,13 @@
+import React, { useState, useRef, UseEffect } from "react";
+import { HiMenu } from "react-icons/hi-menu";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { Link, Route, Routes } from "react-router-dom";
+
+import { Sidebar, UserProfile } from "./components";
+import Pins from "./Pins";
+import { client } from "./client";
+import logo from "../assets/logo.png";
+
 import React from "react";
 
 const Home = () => {
