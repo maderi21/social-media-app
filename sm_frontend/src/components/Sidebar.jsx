e52352177745ab7f5a1fc07b183224b0ce6d26d8
xkostolanskym <kostolansky.marek@gmail.com>
2023-04-19 21:20:19 +0200

    Sidebar

--- sm_frontend/src/components/Sidebar.jsx
+++ sm_frontend/src/components/Sidebar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { RiHomeFill } from "react-icons/ri";
+import { IoIosArrowForward } from "react-icons/io";
 
-const Sidebar = () => {
-  return <div>sidebar</div>;
+import logo from "../assets/logo.png";
+
+const Sidebar = (user, closeToggle) => {
+  const handleCloseSider = () => {
+    if (closeToggle) closeToggle(false);
+  };
+
+  return (
+    <div className="flex flex-col justify-between bg-white h-full overlflow-y-scroll min-w-210 hide-scrollbar">
+      <div className="felx flex-col">
+        <Link
+          to="/"
+          className="flex px-5 gap-2 my-6 py-1 w-190 items-center"
+          onClick={handleCloseSider}
+        >
+          <img src={logo} alt="logo" className="w-full" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
