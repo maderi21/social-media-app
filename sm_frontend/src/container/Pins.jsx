2818ad5f2545f0d5041bff92506bb698b8077c1f
xkostolanskym <kostolansky.marek@gmail.com>
2023-04-20 23:47:46 +0200

    Pins com

--- sm_frontend/src/container/Pins.jsx
+++ sm_frontend/src/container/Pins.jsx
@@ -9,7 +9,11 @@ const Pins = ({ user }) => {
   return (
     <div className="px-2 md:px-5">
       <div className="bg-gray-50">
-        <Navbar searchTerm={searchTerm} setSearchterm={setSearchTerm} />
+        <Navbar
+          searchTerm={searchTerm}
+          setSearchterm={setSearchTerm}
+          user={user}
+        />
       </div>
       <div className="h-full">
         <Routes>
