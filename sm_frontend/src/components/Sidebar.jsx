4ad879ba99300fe89613a0dec902ee0457ae6d16
xkostolanskym <kostolansky.marek@gmail.com>
2023-04-20 22:48:14 +0200

    Sidebar-final

--- sm_frontend/src/components/Sidebar.jsx
+++ sm_frontend/src/components/Sidebar.jsx
@@ -55,10 +55,27 @@ const Sidebar = (user, closeToggle) => {
                 isActive ? isActiveStyle : isNotActiveStyle
               }
               onClick={handleCloseSider}
-            ></NavLink>
+              key={category.name}
+            >
+              {categorie.name}
+            </NavLink>
           ))}
         </div>
       </div>
+      {user && (
+        <Link
+          to={`user-profile/${user._id}`}
+          className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+          onClick={handleCloseSider}
+        >
+          <img
+            src={user.image}
+            className="w-10 h-10 rounded-full"
+            alt="user-profile"
+          />
+          <p>{user.userName}</p>
+        </Link>
+      )}
     </div>
   );
 };
