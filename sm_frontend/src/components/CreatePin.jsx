164c978a65a099a6ec9358de05067d594c9ec2f9
xkostolanskym <kostolansky.marek@gmail.com>
2023-04-25 12:37:02 +0200

    create pin

--- sm_frontend/src/components/CreatePin.jsx
+++ sm_frontend/src/components/CreatePin.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
-const CreatePin = () => {
+import { client } from "../client";
+import Spinner from "./Spinner";
+
+//categoies [{name: 'sports' image: ''}]
+
+const CreatePin = (user) => {
   return <div>CreatePin</div>;
 };
 
