aad1e629a38374f976f7e8f986899251cc85c1a9
xkostolanskym <kostolansky.marek@gmail.com>
2023-04-26 13:32:24 +0200

    input under upload

--- sm_frontend/src/components/CreatePin.jsx
+++ sm_frontend/src/components/CreatePin.jsx
@@ -101,6 +101,44 @@ const CreatePin = (user) => {
             )}
           </div>
         </div>
+        <div className="flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full">
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Add your title here"
+            className="outline-none text-2x1 sm:text-3x1 font-bold border-b-2 border-gray-200 p-2"
+          />
+          {user && (
+            <div className="flex gap-2 my-2 items-center bg-white rounded-lg">
+              <img
+                src={user.image}
+                className="w-10 h-10 rounded-full"
+                alt="user-profile"
+              />
+              <p className="font-bold">{user.userName}</p>
+            </div>
+          )}
+          <input
+            type="text"
+            value={about}
+            onChange={(e) => setAbout(e.target.value)}
+            placeholder="What is your pin about"
+            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+          />
+          <input
+            type="text"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            placeholder="Add a destination link"
+            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+          />
+          <div className="flex flex-col">
+            <div className="mb-2 font-semibold text-lg sm:text-xl">
+              <p>Choose Pin category</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
