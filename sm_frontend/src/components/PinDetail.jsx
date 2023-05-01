da03a95cadbff6d2532f6abcadefe804219acbdc
xkostolanskym <kostolansky.marek@gmail.com>
2023-05-01 21:35:41 +0200

    download

--- sm_frontend/src/components/PinDetail.jsx
+++ sm_frontend/src/components/PinDetail.jsx
@@ -10,12 +10,60 @@ import Spinner from "./Spinner";
 
 const PinDetail = (user) => {
   const [pins, setPins] = useState(null);
-  const [pinDetails, setPinDetails] = useState(null);
-  useState[(comment, setComment)] = useState("");
+  const [pinDetail, setPinDetail] = useState(null);
+  const [comment, setComment] = useState("");
   const [addingComment, setAddingComent] = useState(false);
   const { pinId } = useParams();
 
-  return <div>PinDetail</div>;
+  const fetchPinDetails = () => {
+    let query = pinDetailQuery(pinId);
+
+    if (query) {
+      client.fetch(query).then((data) => {
+        setPinDetail(data[0]);
+
+        if (data[0]) {
+          query = PinDetailMorePinQuery(data[0]);
+          client.fetch(query).then((res) => setPins(res));
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchPinDetails();
+  }, [pinId]);
+
+  if (!pinDetail) return <Spinner message="Loading Pin..." />;
+
+  return (
+    <div
+      className="flex xl-flex-row flex-col m-auto bg-white"
+      style={{ maxWidth: "1500px", borderRadius: "32px" }}
+    >
+      <div className="flex-justify-center items-center md:items-start flex-initial">
+        <img
+          src={pinDetail?.image && urlFor(pinDetail.image).url()}
+          className="rounded-t-3xl rounded-b-lg"
+          alt="user-post"
+        />
+      </div>
+      <div className="w-full p-5 flex-1 xl:min-w-620">
+        <div className="flex items-center justify-between">
+          <div className="flex gap-2 items-center ">
+            <a
+              href={`${image?.asset?.url}?dl=`}
+              download
+              onClick={(e) => e.stopPropagation()}
+              className="bg-white w-9 h-9 rounded-full flex-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+            >
+              <MdDownloadForOffline />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PinDetail;
