836c011cc7bf6341dbafc5e12e9d499bec9f0fa2
xkostolanskym <kostolansky.marek@gmail.com>
2023-05-02 22:32:30 +0200

    pin details final

--- sm_frontend/src/components/Pin.jsx
+++ sm_frontend/src/components/Pin.jsx
@@ -93,7 +93,7 @@ className="bg-white w-9 h-9 rounded-full flex-center justify-center text-dark te
 	className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:100 hover:shadow-md"
 	>
 		<BsFillArrowUpRightCircleFill/>
-		{destination.length > 20? destination.slice(8,20) : destination.slice(8) }
+		{destination.length > 15 ? `${destination.slice(0,15)}...`: destination }
 	</a>
 )}
 {postedBy?._id === user.googleId && (
@@ -113,8 +113,9 @@ className="bg-white w-9 h-9 rounded-full flex-center justify-center text-dark te
 			<p className="font-semibold capitalize">{postedBy?.userName}</p>
 		</Link>
 	</div>
-	)
 	}
+
+
 
 
 
