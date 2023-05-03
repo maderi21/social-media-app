2aca00d0dd53dbb3c445f0eb0c289c04527db8f4
xkostolanskym <kostolansky.marek@gmail.com>
2023-05-03 12:08:47 +0200

    UserProfile imports

--- sm_frontend/src/components/UserProfile.jsx
+++ sm_frontend/src/components/UserProfile.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiOutlineLogout } from "rect-icons/ai";
+import { ueParams, useNavigate, useParams } from "react-router-dom";
+import { GoogleLogout } from "react-google-login";
+
+import {
+  userCreatedPinsQuery,
+  userQuery,
+  userSavedPinsQuery,
+} from "../utils/data";
+import { client } from "../client";
+import MasonryLayout from "./MosonryLayout";
+import Spinner from "./Spinner";
 
 const UserProfile = () => {
+  const [user, setUser] = useState(null);
+  const [pins, setPins] = useState(null);
+  const [tet, setText] = useState("Created");
+  const [activeBtn, setActiveBtn] = useState("created");
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  useEffect(() => {
+    const query = userQuery(userId);
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+    });
+  }, [userId]);
+
+  if (!user) {
+    return <Spinner message="Loading profile..." />;
+  }
+
   return <div>UserProfile</div>;
 };
 
