74ccd80193f8586d98ebe0323167adb02471c34a
xkostolanskym <kostolansky.marek@gmail.com>
2023-05-03 12:20:18 +0200

    googleLogout

--- sm_frontend/src/components/UserProfile.jsx
+++ sm_frontend/src/components/UserProfile.jsx
@@ -12,6 +12,9 @@ import { client } from "../client";
 import MasonryLayout from "./MosonryLayout";
 import Spinner from "./Spinner";
 
+const randomImage =
+  "https://source.unsplash.com/1600x900/?nature,photography,technology";
+
 const UserProfile = () => {
   const [user, setUser] = useState(null);
   const [pins, setPins] = useState(null);
@@ -31,7 +34,50 @@ const UserProfile = () => {
     return <Spinner message="Loading profile..." />;
   }
 
-  return <div>UserProfile</div>;
+  return (
+    <div className="relative pb-2 h-full justify-center items-center">
+      <div className="flex flex-col pb-5">
+        <div className="relative flex flex-col mb-7">
+          <div className="flex flex-col justify-cetner items-center">
+            <img
+              src={randomImage}
+              className="w-full h-370 2xl:h-510 shadow-lg object-cover"
+              alt="banner-pic"
+            />
+            <img
+              className="roudned-full w-20 h-20 -mt-10 shadow-xl object-cover"
+              src={user.image}
+              alt="user-pic"
+            />
+            <h1 className="font-bold text-3xl text-center mt-3">
+              {user.userName}
+            </h1>
+            <div className="absolute top-0 z-1 right-0 p-2">
+              {userId === user._id && (
+                <GoogleLogout
+                  clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+                  render={(renderProps) => (
+                    <button
+                      type="button"
+                      className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      <FcGoogle className="mr-4" /> Sign in with google
+                    </button>
+                  )}
+                  onSuccess={responseGoogle}
+                  onFailure={responseGoogle}
+                  cookiePolicy="single_host_origin"
+                />
+              )}
+              ;
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
